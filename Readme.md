5f066dd9556ce14a8d558571e159b40ba150437b
Timur Iliasov <tilasov391@gmail.com>
2025-12-13 14:46:38 +0300

    init commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Iliasov project 
---
+++ src/main/java/ru/javarush/iliasov/cryptoanalyzer/Runner.java
@@ -0,0 +1,7 @@
+package ru.javarush.iliasov.cryptoanalyzer;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven!");
+    }
+}
