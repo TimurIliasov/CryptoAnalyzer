1228ebb00dfdda2bb72a066fe773fa70dfdb4913
Timur Iliasov <tilasov391@gmail.com>
2025-12-20 23:40:18 +0300

    ADDED Validation to Analyzer.java

--- src/main/java/ru/javarush/iliasov/cryptoanalyzer/commands/Analyzer.java
+++ src/main/java/ru/javarush/iliasov/cryptoanalyzer/commands/Analyzer.java
@@ -2,6 +2,7 @@ package ru.javarush.iliasov.cryptoanalyzer.commands;
 
 import ru.javarush.iliasov.cryptoanalyzer.constants.AnalyzeLogic;
 import ru.javarush.iliasov.cryptoanalyzer.constants.Cipher;
+import ru.javarush.iliasov.cryptoanalyzer.controllers.FileValidation;
 import ru.javarush.iliasov.cryptoanalyzer.entity.Result;
 import ru.javarush.iliasov.cryptoanalyzer.entity.ResultCode;
 
@@ -13,12 +14,22 @@ public class Analyzer implements Action {
 
     @Override
     public Result execute(String[] parameters) throws IOException {
+        if (parameters.length != 3){
+            return new Result("Usage: analyze<encryptedFile> <dictionaryFile> <outputFile>",
+                    ResultCode.ERROR);
+        }
+
         String encryptedFile = parameters[0];
         String dictionaryFile = parameters[1];
         String analyzedFile = parameters[2];
 
         String encryptedText = Files.readString(Path.of(encryptedFile));
         try {
+            FileValidation.validateReadable(encryptedFile);
+            FileValidation.validateReadable(dictionaryFile);
+            FileValidation.validatorWritable(analyzedFile);
+
+            encryptedText = Files.readString(Path.of(encryptedFile));
             AnalyzeLogic.AnalyzeResult analyzeResult = AnalyzeLogic.analyze(encryptedText);
             int decryptKey = analyzeResult.alphabet().length() - analyzeResult.key();
 
